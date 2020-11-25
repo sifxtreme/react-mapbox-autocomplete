4323344e8d477a7fc7f518034c219b2f909bee19
Brian Gonzalez <me@briangonzalez.org>
2020-11-25 15:14:48 -0800

    remove build script

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var _propTypes2 = _interopRequireDefault(_propTypes);
 
 var _lodash = require('lodash');
 
+require('./index.css');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
